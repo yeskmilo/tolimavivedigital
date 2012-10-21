608ea5794aba3311c2218a1fd397a5cfab8aeb66
unknown <yeskmilo@gmail.com>
2012-10-21 12:54:49 -0500

    comienzo del corte y modulos pendientes

--- sites/all/themes/tolimavivedigital/templates/page.tpl.php
+++ sites/all/themes/tolimavivedigital/templates/page.tpl.php
@@ -1,59 +1,100 @@
-<?php if ($page['help'] || ($show_messages && $messages)): ?>
-  <div id='console'><div class='limiter clearfix'>
-    <?php print render($page['help']); ?>
-    <?php if ($show_messages && $messages): print $messages; endif; ?>
-  </div></div>
-<?php endif; ?>
-
-<?php if ($page['header']): ?>
-  <div id='header'><div class='limiter clearfix'>
-    <?php print render($page['header']); ?>
-  </div></div>
-<?php endif; ?>
-
-<div id='branding'><div class='limiter clearfix'>
-  <?php if ($site_name): ?><h1 class='site-name'><?php print $site_name ?></h1><?php endif; ?>
-</div></div>
-
-<div id='navigation'><div class='limiter clearfix'>
-  <?php if (isset($main_menu)) : ?>
-    <?php print theme('links', array('links' => $main_menu, 'attributes' => array('class' => 'links main-menu'))) ?>
-  <?php endif; ?>
-  <?php if (isset($secondary_menu)) : ?>
-    <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('class' => 'links secondary-menu'))) ?>
-  <?php endif; ?>
-</div></div>
-
-<?php if ($page['highlighted']): ?>
-  <div id='highlighted'><div class='limiter clearfix'>
-    <?php print render($page['highlighted']); ?>
-  </div></div>
-<?php endif; ?>
-
-<div id='page'><div class='limiter clearfix'>
-
-  <?php if ($page['sidebar_first']): ?>
-    <div id='left' class='clearfix'><?php print render($page['sidebar_first']) ?></div>
-  <?php endif; ?>
-
-  <div id='main-content' class='clearfix'>
-    <?php if ($breadcrumb) print $breadcrumb; ?>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?><h1 class='page-title'><?php print $title ?></h1><?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php if ($primary_local_tasks): ?><ul class='links clearfix'><?php print render($primary_local_tasks) ?></ul><?php endif; ?>
-    <?php if ($secondary_local_tasks): ?><ul class='links clearfix'><?php print render($secondary_local_tasks) ?></ul><?php endif; ?>
-    <?php if ($action_links): ?><ul class='links clearfix'><?php print render($action_links); ?></ul><?php endif; ?>
-    <div id='content' class='clearfix'><?php print render($page['content']) ?></div>
+<?php
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - regions[header] = Header
+ * -regions[post_header] = Post Header
+*-regions[content] = Content
+*-regions[content_left] = Content Left
+*-regions[content_right] = Content Right
+*-regions[pre_footer] = Pre Footer
+*-regions[footer] = Footer
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ */?>
+<div id="page-wrapper">
+  <div id="top">
+    <div class="p-header">
+      <?php print render($page['header']);?>
+    </div>
+    <div class="p-post-header">
+      <?php print render($page['post_header']); ?>
+    </div>
   </div>
-
-  <?php if ($page['sidebar_second']): ?>
-    <div id='right' class='clearfix'><?php print render($page['sidebar_second']) ?></div>
-  <?php endif; ?>
-
-</div></div>
-
-<div id="footer"><div class='limiter clearfix'>
-  <?php print $feed_icons ?>
-  <?php print render($page['footer']) ?>
-</div></div>
+  <div id="content-wrapper">
+    <div id="pre-content">
+      <?php print render($page['content']); ?>
+    </div>
+    <div id="content">
+      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php if ($messages): ?><h1 class="title" id="page-title"><?php print $messages; ?></h1><?php endif; ?>
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+      <div class="p-content-left">
+        <?php print render($page['content_left']); ?>
+      </div>
+      <div class="p-content-right">
+        <?php print render($page['content_right']); ?>
+      </div>
+    </div>
+  </div>
+  <div id="bottom">
+    <div id="pre-footer">
+      <?php print render($page['pre_footer']); ?>
+    </div>
+    <div id="footer">
+      <?php print render($page['footer']); ?>
+    </div>
+  </div>
+</div>
