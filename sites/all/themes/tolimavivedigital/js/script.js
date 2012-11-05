34482924556161288f2761e4489f6ab9dc42b638
Camilo Ortiz <yeskmilo@gmail.com>
2012-11-04 23:12:44 -0500

    Saliendo de la Ksa de Lili :D

--- sites/all/themes/tolimavivedigital/js/script.js
+++ sites/all/themes/tolimavivedigital/js/script.js
@@ -1,10 +1,18 @@
 (function ($) {
   $(document).ready(function () {
 	  var num = 1;
-		$("#quicktabs-navegacion_tolima_vive_digital .item-list li").each(function(){
+		$("#quicktabs-navegacion_tolima_vive_digital > .item-list li").each(function(){
 			$(this).addClass('menu-'+num);
 			$('a',this).text('');
 			num++;
 		});
+		var num = 1;
+		$("#quicktabs-proyecto > .item-list li").each(function(){
+			$(this).addClass('menu-proyecto-'+num);
+			//$('a',this).text('');
+			num++;
+		});
+		$('.views-slideshow-controls-text-previous a').text('');
+		$('.views-slideshow-controls-text-next a').text('');
   })
 })(jQuery);
