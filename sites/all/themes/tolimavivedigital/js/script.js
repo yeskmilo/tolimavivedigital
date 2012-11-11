4f05e575ecb97a520d3b09be031160ca5bc4cf45
liliana <lilislayer@gmail.com>
2012-11-11 04:12:00 -0500

    adelanto 04:11am 11/11/12

--- sites/all/themes/tolimavivedigital/js/script.js
+++ sites/all/themes/tolimavivedigital/js/script.js
@@ -1,25 +1,31 @@
 (function ($) {
-  $(document).ready(function () {
-	  var num = 1;
-		$("#quicktabs-navegacion_tolima_vive_digital > .item-list li").each(function(){
-			$(this).addClass('menu-'+num);
-			$('a',this).text('');
-			num++;
-		});
-		var num = 1;
-		$("#quicktabs-proyecto > .item-list li").each(function(){
-			$(this).addClass('menu-proyecto-'+num);
-			//$('a',this).text('');
-			num++;
-		});
+	Drupal.behaviors.tolimavivedigital = {
+    attach: function (context, settings) {
+      var num = 1;
+			$("#quicktabs-navegacion_tolima_vive_digital > .item-list li").each(function(){
+				$(this).addClass('menu-'+num);
+				$('a',this).text('');
+				num++;
+			});
+			var num = 1;
+			$("#quicktabs-proyecto > .item-list li").each(function(){
+				$(this).addClass('menu-proyecto-'+num);
+				//$('a',this).text('');
+				num++;
+			});
 
-		var num = 1;
-		$("#quicktabs-servicios > .item-list li").each(function(){
-			$(this).addClass('menu-servicios-'+num);
-			//$('a',this).text('');
-			num++;
-		});
-		$('.views-slideshow-controls-text-previous a').text('');
-		$('.views-slideshow-controls-text-next a').text('');
-  })
+			var num = 1;
+			$("#quicktabs-servicios > .item-list li").each(function(){
+				$(this).addClass('menu-servicios-'+num);
+				//$('a',this).text('');
+				num++;
+			});
+			$('.views-slideshow-controls-text-previous a').text('');
+			$('.views-slideshow-controls-text-next a').text('');
+			$('.view-galeria-home .group-right .field-name-body').jScrollPane();
+    }
+  };
+  /*$(document).ready(function () {
+	  
+  })*/
 })(jQuery);
